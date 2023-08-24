a258ff0df65d6168249a92721023338f08e04fcf
Temis Pita <temispita9000@gmail.com>
2023-08-24 11:48:57 -0400

    feat: proyecto formulario

---
+++ spring-boot-form/src/test/java/com/curso/springbootform/form/app/SpringBootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springbootform.form.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
