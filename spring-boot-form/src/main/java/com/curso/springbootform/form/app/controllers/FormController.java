61b38ee06fa5eb5ed10d3d7f3d6792396846fc77
Temis Barreto <temispita9000@gmail.com>
2023-08-28 10:02:50 -0400

    feat: avances

--- spring-boot-form/src/main/java/com/curso/springbootform/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/curso/springbootform/form/app/controllers/FormController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,12 +18,17 @@ public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("John");
+        usuario.setApellido("Doe");
+        usuario.setIdentificador("123.123.123-k");
         model.addAttribute("titulo", "Formulario de usuarios");
+        model.addAttribute("user",usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model)
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model)
 //                           @RequestParam(name = "username") String username,
 //                           @RequestParam String password,
 //                           @RequestParam String email)
@@ -36,12 +42,12 @@ public class FormController {
         model.addAttribute("titulo", "Resultado del formulario");
 
         if (result.hasErrors()){
-            Map<String,String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(err ->
-                    errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage())
-                    )
-            );
-            model.addAttribute("error",errores);
+//            Map<String,String> errores = new HashMap<>();
+//            result.getFieldErrors().forEach(err ->
+//                    errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage())
+//                    )
+//            );
+//            model.addAttribute("error",errores);
             return "form";
         }
         model.addAttribute("usuario", usuario);
