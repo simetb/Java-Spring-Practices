c0fb957b029d478de93868c309bd95f95eee6e0b
Temis Barreto <temispita9000@gmail.com>
2023-10-04 10:02:07 -0400

    avanaces

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email,create_at) VALUES(1,'Andres','Guzman','pruebas@gmail.com','2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email,create_at) VALUES(2,'Jose','Valdez','pruebasjose@gmail.com','2018-09-29');
